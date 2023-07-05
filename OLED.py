d0905573f04448cd870d550e3104a499cd653d56
Pehnny <124045706+Pehnny@users.noreply.github.com>
2023-07-05 10:44:24 +0200

    Add files via upload

--- OLED.py
+++ OLED.py
@@ -1,22 +1,18 @@
-from time import time
-from reseau import Lattice
-from plot import plot
+from reseau import *
 
+def IQE(proportions : list[float], dimension : tuple[int, int, int], charges : int) -> float :
+    oled : Lattice = Lattice(proportions, dimension=dimension, charges=charges)
+    recombinations : int = 10**3
+    oled.operations(recombinations)
+    print(f"dimensions : {dimension}")
+    print(f"charges : {charges}")
+    print(f"injections : {oled._injection}")
+    print(f"recombinations : {oled._recombination}")
+    print(f"emissions : {oled._emission}")
+    print(f"IQE : {oled.get_IQE()}")
+    return 100. - oled.get_IQE()
 
-dimensions = (10,10,5)
 proportions = (0.84,0.15,0.01)
-OP = 10**2
-start = time()
-test = Lattice(dimensions, proportions, charges = 4)
-test.operations(OP)
-# for i in range(OP) :
-#     electrons, holes, excitons = test.get_particules_positions()
-#     plot(electrons, holes, excitons, *dimensions, str(i))
-#     test.operations(1)
-# electrons, holes, excitons = test.get_particules_positions()
-# plot(electrons, holes, excitons, *dimensions, str(OP))
-end = time()
-print(f"IQE : {test.get_IQE()}")
-print(f"emissions : {test._emission}")
-print(f"injections : {test._injection}")
-print(f"{end - start} s")
+dimension = (20,20,20)
+charges = 4
+IQE(proportions, dimension, charges)
