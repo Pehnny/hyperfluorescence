b249d054917d070ad9ee3aadfe1d3235d3b12f52
Pehnny <124045706+Pehnny@users.noreply.github.com>
2023-07-11 10:49:14 +0200

    Delete constants.py

--- constants.py
+++
@@ -1,6 +0,0 @@
-from math import pi
-
-BOLTZMANN : float = 8.617333262 * 10.**(-5)                                              # [eV/K]
-VACUUM_PERMITTIVITY : float = 55263.49406                                               # [e²/eV.nm]
-RELATIVE_PERMITTIVITY : float = 3.
-ELECTROSTATIC : float = 1. / (4. * pi * VACUUM_PERMITTIVITY * RELATIVE_PERMITTIVITY)    # [eV.nm]
